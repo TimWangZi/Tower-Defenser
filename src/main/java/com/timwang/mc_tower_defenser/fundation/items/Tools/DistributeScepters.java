c7bce857c59b7b411abcfd91614a3bdeebda1d49
TimWangZi <53970763+TimWangZi@users.noreply.github.com>
2026-04-07 17:43:59 +0800

    2026/4/7 17:43

---
+++ src/main/java/com/timwang/mc_tower_defenser/fundation/items/Tools/DistributeScepters.java
@@ -0,0 +1,9 @@
+package com.timwang.mc_tower_defenser.fundation.items.Tools;
+
+import net.minecraft.world.item.Item;
+
+public class DistributeScepters extends Item {
+    public DistributeScepters(Properties properties) {
+        super(properties);
+    }
+}
